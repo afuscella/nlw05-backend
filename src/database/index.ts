9c993f86f419cc5e547137dd416373a21f2d5a47
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-20 23:42:09 -0300

    feat(database): config sqlite database

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import { createConnection, Repository } from 'typeorm';
+
+export const Database = {
+  async setup(createConnectionModule = createConnection) {
+    try {
+      return await createConnectionModule();
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
+}
