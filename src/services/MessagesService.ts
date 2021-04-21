55050bdee9dce2a2886d3dcb14524b3f7ab4ed6a
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-21 18:01:19 -0300

    feat(services): add Messages service layer

---
+++ src/services/MessagesService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from 'typeorm';
+import { Message } from '../models/entities/Message';
+import { MessagesRepository } from '../repositories/MessagesRepository';
+
+interface ICreate {
+  admin_id?: string;
+  text: string;
+  user_id: string;
+}
+
+export interface IMessagesService {
+  handleCreate: ({ admin_id, text, user_id }: ICreate) => Promise<Message>
+  handleIndex(user_id: string): Promise<Message[]>
+}
+
+export class MessagesService implements IMessagesService {
+  constructor(private messagesRepository = getCustomRepository(MessagesRepository)) {
+
+  }
+  async handleCreate({ admin_id, text, user_id }: ICreate) {
+    const message = this.messagesRepository.create({
+      admin_id,
+      text,
+      user_id,
+    });
+    await this.messagesRepository.save(message);
+    return message;
+  }
+
+  async handleIndex(user_id: string) {
+    const messages = await this.messagesRepository.find({
+      where: { user_id },
+      relations: ['user']
+    });
+    return messages;
+  }
+}
