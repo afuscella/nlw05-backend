c89d92c9e79b6b40f6907d6f6b5ac467b11aefd3
Arthur Silva <fuscellaarthur@gmail.com>
2021-04-21 18:04:57 -0300

    feat(routes): config app routes for user, messages and settings

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
-import { SettingController } from './controllers/SettingsController';
+import { MessagesController } from './controllers/MessagesController';
+import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 
 const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const controller = new UsersController();
+  return controller.create(request, response);
+});
+
 routes.post('/settings', (request, response) => {
-  const controller = new SettingController();
+  const controller = new SettingsController();
+  return controller.create(request, response);
+});
+
+routes.get('/messages/:id', (request, response) => {
+  const controller = new MessagesController();
+  return controller.index(request, response);
+});
+
+routes.post('/messages', (request, response) => {
+  const controller = new MessagesController();
   return controller.create(request, response);
 });
 
